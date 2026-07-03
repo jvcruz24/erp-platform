e0843be7ae0e7cb341377edb00397e66b0bed4df
jv <jvcruz1998@gmail.com>
2026-07-03 11:09:49 +0800

    removed api with fastify

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,8 +2,8 @@ import styles from './page.module.css';
 import { prisma } from '@repo/db';
 
 export default async function Home() {
+  // TODO MAIN
   const user = await prisma;
-  console.log('Prisma: ', user);
   return (
     <div className={styles.page}>
       <main className={styles.main}></main>
